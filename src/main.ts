38ba27aeda0e35c74e4f853260a55165633d2bf0
guillaume <guillaume-duclos@hotmail.fr>
2022-11-14 23:10:49 +0100

    WIP

--- src/main.ts
+++ src/main.ts
@@ -10,25 +10,21 @@ card.addEventListener('mousemove', (event: Event): void => {
   let angleY;
 
   if (position.offsetY > card.clientHeight / 2) {
-    angleX = 1 - (card.clientHeight - position.offsetY) / (card.clientHeight / 2);
-    console.log(1 - (card.clientHeight - position.offsetY) / (card.clientHeight / 2));
+    angleX = 10 - (card.clientHeight - position.offsetY) / (card.clientHeight / 2) * 10;
   } else {
-    angleX = -1 - (1 - (card.clientHeight) / (card.clientHeight - position.offsetY));
-    //console.log(-1 - (1 - (card.clientHeight) / (card.clientHeight - position.offsetY)));
+    angleX = -10 - (1 - (card.clientHeight) / (card.clientHeight - position.offsetY)) * 10;
   }
 
   if (position.offsetX > card.clientWidth / 2) {
-    angleY = 1 - (card.clientWidth - position.offsetX) / (card.clientWidth / 2);
-    //console.log(1 - (card.clientWidth - position.offsetX) / (card.clientWidth / 2));
+    angleY = -10 + (card.clientWidth - position.offsetX) / (card.clientWidth / 2) * 10;
   } else {
-    angleY = -1 - (1 - (card.clientWidth) / (card.clientWidth - position.offsetX));
-    //console.log(-1 - (1 - (card.clientWidth) / (card.clientWidth - position.offsetX)));
+    angleY = Math.abs(10 - (card.clientWidth - position.offsetX) / (card.clientWidth / 2) * 10);
   }
 
-  card.style.transform = `rotate3d(${angleX}, ${angleY}, 0, 20deg)`;
+  card.style.transform = `rotateX(${angleX}deg) rotateY(${angleY}deg)`;
 });
 
 card.addEventListener('mouseout', (): void => {
   card.style.transition = 'transform .2s ease-in-out';
-  card.style.transform = `rotate3d(0, 0, 0, 20deg)`;
+  card.style.transform = 'rotateX(0) rotateY(0)';
 });
