88990bdec57933da9ba25e435d923e7b91c52f4c
guillaume <guillaume-duclos@hotmail.fr>
2022-11-26 11:33:54 +0100

    WIP

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import './style.css';
 
 const card: HTMLElement = document.querySelector('.card');
+let root = document.documentElement;
+
+console.log('OK');
+
+window.addEventListener("deviceorientation", handleOrientation, true);
+
+function handleOrientation(event: DeviceOrientationEvent) {
+  console.log(event.absolute);
+  console.log(event.alpha);
+  console.log(event.beta);
+  console.log(event.gamma);
+}
 
 card.addEventListener('mousemove', (event: Event): void => {
   card.style.transition = 'transform 0s ease-in-out';
@@ -8,18 +20,32 @@ card.addEventListener('mousemove', (event: Event): void => {
   const position = event as MouseEvent;
   let angleX;
   let angleY;
+  let positionX = position.offsetX / card.clientWidth * 100;
+  let positionY = position.offsetY / card.clientHeight * 100;
+
+  root.style.setProperty('--positionX', `${positionX}%`);
+  root.style.setProperty('--positionY', `${positionY}%`);
+
+  let posX;
+  let posY;
 
   if (position.offsetY > card.clientHeight / 2) {
     angleX = 10 - (card.clientHeight - position.offsetY) / (card.clientHeight / 2) * 10;
+    posY = 1 - (card.clientHeight - position.offsetY) / (card.clientHeight / 2);
   } else {
     angleX = -10 - (1 - (card.clientHeight) / (card.clientHeight - position.offsetY)) * 10;
+    posY = 1 + (1 - (card.clientHeight) / (card.clientHeight - position.offsetY));
   }
 
   if (position.offsetX > card.clientWidth / 2) {
     angleY = -10 + (card.clientWidth - position.offsetX) / (card.clientWidth / 2) * 10;
+    posX = 1 - (card.clientWidth - position.offsetX) / (card.clientWidth / 2);
   } else {
     angleY = Math.abs(10 - (card.clientWidth - position.offsetX) / (card.clientWidth / 2) * 10);
+    posX = Math.abs(1 - (card.clientWidth - position.offsetX) / (card.clientWidth / 2));
   }
+
+  root.style.setProperty('--hyp', `${posX > posY ? posX : posY}`);
 
   card.style.transform = `rotateX(${angleX}deg) rotateY(${angleY}deg)`;
 });
